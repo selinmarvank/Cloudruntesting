42b080471132994c0a233c77a11296eb8308dbff
Arifanazrin17 <arifanazrinn@gmail.com>
2025-08-28 23:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def get_data():
     client = bigquery.Client()
 
     query = """
-        SELECT username, Age
-        FROM `acoustic-apex-469415-m0.cart_data.user-age`
+        SELECT AgeGroup, TotalCustomers,TotalRevenue
+        FROM `acoustic-apex-469415-m0.DAG.age_summary`
         LIMIT 10
     """
     query_job = client.query(query)  
     results = query_job.result()
 
-    rows = [{"username": row.username, "Age": row.Age} for row in results]
+    rows = [{"AgeGroup": row.AgeGroup, "TotalCustomers": row.TotalCustomers, "TotalRevenue": row.TotalRevenue} for row in results]
     return jsonify(rows)
 
 if __name__ == "__main__":
